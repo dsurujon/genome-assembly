81cf5997ed77d1f55051e008dbad0283eb552e95
Defne <defnesurujon@gmail.com>
2016-07-22 16:54:02 -0400

    Update assemble_genome.py

--- assemble_genome.py
+++ assemble_genome.py
@@ -3,7 +3,7 @@
 
 __author__ = "Defne Surujon"
 __credits__=["Defne Surujon","Alexander Farrell","Federico Rosconi","Jon Anthony","Tim van Opijnen"]
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __maintainer__="Defne Surujon"
 __email__="surujon@bc.edu"
 
